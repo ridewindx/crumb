77d93bcfcc2541558b6b724f97b2b34e2948df29
RideWindX <ridewindx@163.com>
2017-04-05 15:23:07 +0800

    Update channelqueue_test.go

--- queue/channelqueue_test.go
+++ queue/channelqueue_test.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"sync"
 )
 
 func TestChannelQueuePush(t *testing.T) {
@@ -51,3 +52,96 @@ func TestChannelQueuePop(t *testing.T) {
 	assert.Equal(t, `2`, result)
 }
 
+func BenchmarkChannelQueuePushPop(b *testing.B) {
+	rq := NewChannelQueue(64)
+
+	var count int
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			_, err := rq.Pop()
+			assert.Nil(b, err)
+
+			count++
+			if count == b.N {
+				return
+			}
+		}
+	}()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rq.Push(i)
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkChannelQueuePush(b *testing.B) {
+	rq := NewChannelQueue(b.N)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rq.Push(i)
+	}
+}
+
+func BenchmarkChannelQueuePop(b *testing.B) {
+	rq := NewChannelQueue(b.N)
+
+	for i := 0; i < b.N; i++ {
+		err := rq.Push(i)
+		assert.Nil(b, err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rq.Pop()
+	}
+}
+
+func BenchmarkChannelQueueParallelPushPop(b *testing.B) {
+	rq := NewChannelQueue(b.N)
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			rq.Push(i)
+			rq.Pop()
+			i++
+		}
+	})
+}
+
+func BenchmarkChannelQueueParallelPush(b *testing.B) {
+	rq := NewChannelQueue(b.N)
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			rq.Push(i)
+			i++
+		}
+	})
+}
+
+func BenchmarkChannelQueueParallelPop(b *testing.B) {
+	rq := NewChannelQueue(b.N)
+
+	for i := 0; i < b.N; i++ {
+		err := rq.Push(i)
+		assert.Nil(b, err)
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			rq.Pop()
+		}
+	})
+}
