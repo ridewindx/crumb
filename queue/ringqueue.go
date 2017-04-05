5763a204f03eeaba5412f99f48c7df0b567f8872
RideWindX <ridewindx@163.com>
2017-04-05 15:23:15 +0800

    Update ringqueue.go

--- queue/ringqueue.go
+++ queue/ringqueue.go
@@ -3,7 +3,6 @@ package queue
 import (
 	"runtime"
 	"sync/atomic"
-	"fmt"
 	"time"
 )
 
@@ -31,20 +30,20 @@ type ringnode struct {
 // NewRingQueue will allocate a RingQueue with the specified capacity.
 // The `spin` specifies the waiting strategy when operation will be blocked:
 // if true, spin on CAS; otherwise, wait on channel for notification.
-func NewRingQueue(capacity uint64, spin bool) *RingQueue {
+func NewRingQueue(capacity int, spin bool) *RingQueue {
 	if capacity == 0 {
 		panic("RingQueue capacity must be greater than 0")
 	}
-	capacity = roundUp(capacity)
+	n := roundUp(uint64(capacity))
 
 	rb := &RingQueue{
-		nodes: make([]*ringnode, capacity),
-		mask: capacity-1,
+		nodes: make([]*ringnode, n),
+		mask: n-1,
 		spin: spin,
 		notFull: make(chan struct{}, 1),
 		notEmpty: make(chan struct{}, 1),
 	}
-	for i := uint64(0); i < capacity; i++ {
+	for i := uint64(0); i < n; i++ {
 		rb.nodes[i] = &ringnode{position: i}
 	}
 	return rb
@@ -54,45 +53,50 @@ func NewRingQueue(capacity uint64, spin bool) *RingQueue {
 // until an item is added to the queue. If a nonzero timeout is specified,
 // block no more than the timeout duration and return ErrTimeout. If timeout
 // is zero, immediately return ErrFull.
-func (rb *RingQueue) Push(item interface{}, timeout ...time.Duration) error {
+func (rq *RingQueue) Push(item interface{}, timeout ...time.Duration) error {
 	var n *ringnode
 	var pos uint64
+	var tic time.Time
+	if len(timeout) > 0 && timeout[0] > 0 {
+		tic = time.Now()
+	}
 	i := 0
 	for {
-		pos = atomic.LoadUint64(&rb.tail)
-		n = rb.nodes[pos&rb.mask]
+		pos = atomic.LoadUint64(&rq.tail)
+		n = rq.nodes[pos&rq.mask]
 		seq := atomic.LoadUint64(&n.position)
-		switch dif := int64(seq) - int64(pos); {
-		case dif == 0:
-			if atomic.CompareAndSwapUint64(&rb.tail, pos, pos+1) {
+		if seq == pos {
+			if atomic.CompareAndSwapUint64(&rq.tail, pos, pos+1) {
 				n.data = item
 				atomic.StoreUint64(&n.position, pos+1)
-				rb.print()
-				if !rb.spin {
+				//rq.print()
+				if !rq.spin {
 					select {
-					case rb.notEmpty <- struct{}{}:
+					case rq.notEmpty <- struct{}{}:
 					default:
 					}
 				}
 				return nil
 			}
-		case dif < 0: // queue is full
+		} else if seq < pos { // queue is full
 			if len(timeout) == 0 {
-				if !rb.spin {
-					<-rb.notFull // wait for a pop
+				if !rq.spin {
+					<-rq.notFull // wait for a pop
 				}
 			} else if timeout[0] > 0 {
-				if !rb.spin {
+				if !rq.spin {
 					select { // wait for a pop, until timeout
-					case <-rb.notFull:
+					case <-rq.notFull:
 					case <-time.After(timeout[0]):
 						return ErrTimeout
 					}
+				} else if time.Now().Sub(tic) >= timeout[0] {
+					return ErrTimeout
 				}
 			} else {
 				return ErrFull
 			}
-		default: // another push occurred
+		} else { // another push occurred
 		}
 
 		if i == 10000 {
@@ -108,44 +112,52 @@ func (rb *RingQueue) Push(item interface{}, timeout ...time.Duration) error {
 // block until an item can be returned. If a nonzero timeout is specified,
 // block no more than the timeout duration and return ErrTimeout. If timeout
 // is zero, immediately return ErrEmpty.
-func (rb *RingQueue) Pop(timeout ...time.Duration) (interface{}, error) {
+func (rq *RingQueue) Pop(timeout ...time.Duration) (interface{}, error) {
 	var n *ringnode
-	pos := atomic.LoadUint64(&rb.head)
+	pos := atomic.LoadUint64(&rq.head)
+	var tic time.Time
+	if len(timeout) > 0 && timeout[0] > 0 {
+		tic = time.Now()
+	}
 	i := 0
 	for {
-		pos = atomic.LoadUint64(&rb.head)
-		n = rb.nodes[pos&rb.mask]
+		pos = atomic.LoadUint64(&rq.head)
+		n = rq.nodes[pos&rq.mask]
 		seq := atomic.LoadUint64(&n.position)
-		switch dif := int64(seq) - (int64(pos) + 1); {
-		case dif == 0:
-			if atomic.CompareAndSwapUint64(&rb.head, pos, pos+1) {
+		if seq == pos+1 {
+			if atomic.CompareAndSwapUint64(&rq.head, pos, pos+1) {
 				data := n.data
 				n.data = nil
-				atomic.StoreUint64(&n.position, pos+rb.mask+1)
-				rb.print()
-				select {
-				case rb.notFull <- struct{}{}:
-				default:
+				atomic.StoreUint64(&n.position, pos+rq.mask+1)
+				//rq.print()
+				if !rq.spin {
+					select {
+					case rq.notFull <- struct{}{}:
+					default:
+					}
 				}
 				return data, nil
 			}
-		case dif < 0: // queue is empty
+
+		} else if seq < pos+1 { // queue is empty
 			if len(timeout) == 0 {
-				if !rb.spin {
-					<-rb.notEmpty // wait for a push
+				if !rq.spin {
+					<-rq.notEmpty // wait for a push
 				}
 			} else if timeout[0] > 0 {
-				if !rb.spin {
+				if !rq.spin {
 					select { // wait for a push, until timeout
-					case <-rb.notEmpty:
+					case <-rq.notEmpty:
 					case <-time.After(timeout[0]):
 						return nil, ErrTimeout
 					}
+				} else if time.Now().Sub(tic) >= timeout[0] {
+					return nil, ErrTimeout
 				}
 			} else {
 				return nil, ErrEmpty
 			}
-		default: // another pop occurred
+		} else { // another pop occurred
 		}
 
 		if i == 10000 {
@@ -158,8 +170,8 @@ func (rb *RingQueue) Pop(timeout ...time.Duration) (interface{}, error) {
 }
 
 // Len returns the number of items in the queue.
-func (rb *RingQueue) Len() uint64 {
-	return atomic.LoadUint64(&rb.tail) - atomic.LoadUint64(&rb.head)
+func (rq *RingQueue) Len() int {
+	return int(atomic.LoadUint64(&rq.tail) - atomic.LoadUint64(&rq.head))
 }
 
 // Empty returns whether the queue is empty.
@@ -181,6 +193,7 @@ func roundUp(v uint64) uint64 {
 	return v
 }
 
+/*
 func (rq *RingQueue) print() {
 	fmt.Printf("queue %d, dequeue %d\n", rq.tail, rq.head)
 	fmt.Print("nodes: ")
@@ -189,3 +202,4 @@ func (rq *RingQueue) print() {
 	}
 	fmt.Println()
 }
+*/
